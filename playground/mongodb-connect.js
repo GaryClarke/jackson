6e042b9927db4e7669f0e76008fd30469088a29b
Gary Clarke <garyclarke1974@gmail.com>
2018-10-21 22:42:00 +0100

    Lecture 65

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
@@ -21,21 +22,21 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-
-        name: 'Gary',
-        age: 44,
-        location: 'Wilmslow, Cheshire'
-
-    }, (err, result) => {
-
-        if (err) {
-
-            return console.log('Unable to insert todo', err);
-        }
-
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
-
+    // db.collection('Users').insertOne({
+    //
+    //     name: 'Gary',
+    //     age: 44,
+    //     location: 'Wilmslow, Cheshire'
+    //
+    // }, (err, result) => {
+    //
+    //     if (err) {
+    //
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
+    //
     db.close();
 });
