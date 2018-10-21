79845fc23e3529aaf1d38d9a3673d58a12234580
Gary Clarke <garyclarke1974@gmail.com>
2018-10-21 23:33:12 +0100

    Lecture 66

---
+++ playground/mongodb-find.js
@@ -0,0 +1,43 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+    if (err) {
+
+        return console.log('Unable to connect to mongodb server');
+    }
+
+    console.log('Connected to mongodb server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5bccf6445567e1553284df63')
+    // }).toArray().then((docs) => {
+    //
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    //
+    // }, (err) => {
+    //
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //
+    //     console.log(`Todos count: ${count}`);
+    //
+    // }, (err) => {
+    //
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Gary'}).count().then((count) => {
+
+        console.log(`Users count: ${count}`);
+
+    }, (err) => {
+
+        console.log('Unable to fetch users', err);
+    });
+
+    // db.close();
+});
